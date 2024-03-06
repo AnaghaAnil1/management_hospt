103f8ea62c70cb92a97c0de8ab63cd09d04060a9
Anagha Anil <anaghaanil0002@gmail.com>
2024-03-06 20:06:04 +0000

    new commit3

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Departments
 # Create your views here.
 def index(request):
 
@@ -16,7 +16,10 @@ def doctor(request):
     return render(request,'doctor.html')
 
 def dept(request):
-    return render(request,'department.html')
+    dict_deptt={
+        'deptt': Departments.objects.all()
+    }
+    return render(request,'department.html', dict_deptt)
 
 def contact(request):
     return render(request,'contact.html')
