b4daaaeb5f1f0f1fdd9a97e99b832c456d2ce420
Anagha Anil <anaghaanil0002@gmail.com>
2024-03-25 13:35:09 +0000

    new commit9

--- hosp_mana/settings.py
+++ hosp_mana/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-!9r=ir^1uz8tpn3h&7fluh&62a4-&oqh-s&40h-hg(-zr&fd6%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
